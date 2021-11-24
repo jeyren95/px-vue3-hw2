dee8c62790a6af3c65ef75a1e1f83cdce2ffc49b
Jey-ren <jeyren95@hotmail.com>
2021-11-24 18:45:58 +0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,95 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main class="bg-gray-50 lg:flex">
+    <div class="flex-1">
+      <div class="max-w-7xl mx-auto pt-16 pb-24 px-4 sm:px-6 lg:px-8">
+        <div class="sm:flex sm:flex-col sm:align-center mb-12">
+          <h1 class="text-5xl font-extrabold text-gray-900 sm:text-center">Marketplace</h1>
+        </div>
+        <div>
+          <Button 
+          type="button"
+          class="border-transparent  text-white bg-pink-600 hover:bg-pink-700"
+          >
+            <ArrowCircleLeftIcon class="h-5 w-5 inline-block" />
+            Previous
+          </Button>
+          <span class="text-gray-900 sm:text-center">Page {{ page }}</span>
+          <Button 
+          type="button"
+          class="border-transparent  text-white bg-pink-600 hover:bg-pink-700"
+          >
+            <ArrowCircleRightIcon class="h-5 w-5 inline-block" />
+            Next
+          </Button>
+        </div>
+        <div v-if="items.length > 0" class="grid md:grid-cols-2 gap-x-4 gap-y-8 xl:grid-cols-3 xl:gap-x-6">
+          <Item 
+          v-for="(item, index) in items"
+          :key="index"
+          :title="item.title"
+          :imageUrl="item.imageUrl"
+          :price="item.price"
+          :description="item.description"
+          :availability="item.availability"
+          :numOfStock="item.availability !== 'single-item' && item.numOfStock"
+          :condition="item.condition"
+          />
+        </div>
+        <h1 v-else>Loading items...</h1>
+      </div>
+    </div>
+
+    <div class="flex-initial bg-white w-full lg:max-w-md border-b border-gray-100">
+
+    </div>
+
+
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Item from "./components/Item.vue"
+import { ArrowCircleLeftIcon, ArrowCircleRightIcon } from "@heroicons/vue/solid"
 
 export default {
   name: 'App',
+  data() {
+    return {
+      items: [],
+      page: 1
+    }
+  },
   components: {
-    HelloWorld
+    Item,
+    ArrowCircleLeftIcon,
+    ArrowCircleRightIcon
+  },
+  methods: {
+    fetchItems(page, signal) {
+      const response = fetch(`https://ecomm-service.herokuapp.com/marketplace?limit=6&page=${page}`, {signal})
+      .then((res) => res.json())
+
+      return response
+    }
+  },
+  mounted() {
+    const controller = new AbortController()
+
+    this.fetchItems(this.page, controller.signal)
+    .then((data) => this.items = data)
+    .catch((err) => console.log(err))
+
+    return () => controller.abort()
+  },
+  updated() {
+    const controller = new AbortController()
+
+    this.fetchItems(this.page, controller.signal)
+    .then((data) => this.items = data)
+    .catch((err) => console.log(err))
+
+    return () => controller.abort()
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Item.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="relative flex flex-col">
+    <div
+    class="
+      group
+      block
+      w-full
+      rounded-lg
+      bg-gray-100
+      focus-within:ring-2
+      focus-within:ring-offset-2
+      focus-within:ring-offset-gray-100
+      focus-within:ring-pink-500
+      overflow-hidden
+    "
+    >
+      <img :src="imageUrl" class="object-cover pointers-event-none group-hover:opacity-75 h-48 w-full" alt />
+    </div>
+
+    <div class="flex-1 flex md:flex-col justify-between items-start md:items-stretch gap-3 px-2">
+      <div class="mt-1 flex-1">
+        <div class="flex justify-between items-center gap-3">
+          <div>
+            RM 
+            <span class="text-2xl font-bold">{{ price }}</span>
+          </div>
+          <div class="text-sm text-gray-500">
+            {{ numOfStock }} piece available
+          </div>
+        </div>
+      </div>
+
+      <p class="block text-sm font-medium text-gray-900 truncate pointer-events-none">
+        {{ title }}
+      </p>
+      
+      <p class="block text-sm font-medium text-gray-500 pointer-events-none">
+        {{ description }}
+      </p>
+
+
+      <div class="flex flex-col md:flex-row gap-3 py-3">
+        <Button type="button" class="border-transparent  text-white bg-pink-600 hover:bg-pink-700">
+          <PencilIcon class="h-5 w-5 mr-2" />  
+          Edit
+        </Button>
+        <Button type="button" class="border-pink-500 text-pink-500 bg-white hover:text-pink-700">
+          <TrashIcon class="h-5 w-5 mr-2" />
+          Delete
+        </Button>
+      </div>
+
+    </div>
+
+
+  </div>
+</template>
+
+
+<script>
+import Button from "./Button.vue"
+import { TrashIcon, PencilIcon } from "@heroicons/vue/solid"
+
+
+export default {
+  components: {
+    Button,
+    TrashIcon,
+    PencilIcon
+  },
+  props: ["title", "imageUrl", "description", "price", "availability", "numOfStock", "condition"]
+}
+
+</script>
